6e9fa8046143d0d7a39cc745eac8e15acdba650b
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-06-01 15:15:15 +0200

    Update reverseshell.py

--- reverseshell.py
+++ reverseshell.py
@@ -4,7 +4,7 @@ import os
 
 # # # # # # # # # # # # # # # # # # # # # #
 #                                         #
-#   Author  :   Keyjeek                   #
+#   Author  :   Keyj33k                   #
 #   Contact :   nomotikag33n@gmail.com    #
 #   Github  :   @keyj33k                  #
 #   Version :   1.0.4                     #
